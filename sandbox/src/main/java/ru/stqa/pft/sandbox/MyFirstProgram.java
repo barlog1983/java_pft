a7ba960ae41eb49f0df4e0c0d2e3c60bd4ead47c
barlog1983 <osipovp@list.ru>
2018-04-06 17:07:19 +0300

    основы программирования часть 1

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		hello("world");
+		hello("user");
+		hello("Pavel");
+
+		Square s = new Square(5);
+		s.l =5;
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello," + somebody + "!");
+	}
+
+
+
+
+
+}
